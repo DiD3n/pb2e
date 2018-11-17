c369ba5835ad12c5ffaac0e9776d977ff32c9ea4
DiD3n <32616724+DiD3n@users.noreply.github.com>
2018-11-17 18:51:09 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
+#include "logger.hpp"
+
 int main(int argc, char *argv[]) {
 
-    std::cout << "Hello world!" << '\n';
-
+    log("Hello","world!");
+    
     std::cin.get();
     return EXIT_SUCCESS;
 }
