49b399143b5d6bc6d1b446e0dc01e4ce7b00b764
DiD3n <den190242@gmail.com>
2018-11-17 23:19:24 +0100

    sdl include and logError

--- src/logger.hpp
+++ src/logger.hpp
@@ -2,26 +2,30 @@
 
 #include <iostream>
 #include <sstream>
+#include "include/sdl2.hpp"
 
 #define DEBUG
 
 #ifdef DEBUG
-#define log(args...) _raw(args, "||", __FILE__, ":", __LINE__)
+#define log(args...) _raw(args, " :", __FILE__, "\b:",__LINE__)
+#define logError(args...) if('\0' != *SDL_GetError()){\
+_raw(args, " at", __FILE__, "\b:", __LINE__, "\n", SDL_GetError());SDL_ClearError();}else \
+_raw(args, " at", __FILE__, "\b:",__LINE__)
 #else
 #define log(args...);
+#define logError(args...);
 #endif
 
 #define _OUT_LOG std::cout
 
-
 static inline void _print() {
     _OUT_LOG << '\n';
-};
+}
 
 template<typename T, typename ... T2>
 static inline void _print(const T& arg, const T2& ... args) {
     // std::stringstream ss;
-    // ss << arg;  //for a moment when the console will be built-in
+    // ss << arg;
     // _OUT_LOG << ss.str() << " ";
     _OUT_LOG << arg << " ";
     _print(args...);
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 #include "logger.hpp"
 
 int main(int argc, char *argv[]) {
-
+    logError("TEST");
+    SDL_RenderClear(NULL);
     log("Hello","world!");
+    logError("TEST");
+    logError("TEST");
 
     std::cin.get();
     return EXIT_SUCCESS;
