5da41e14e54a81940af2f95bd82750a99a5511d2
DiD3n <den190242@gmail.cpm>
2018-11-17 23:33:55 +0100

    fast fix #2

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <string>
 
 #include "logger.hpp"
 
 int main(int argc, char *argv[]) {
-    logError("TEST");
-    SDL_RenderClear(NULL);
-    log("Hello","world!");
-    logError("TEST");
-    logError("TEST");
-
-    std::cin.get();
-    return EXIT_SUCCESS;
-=======
-#include <iostream>
-#include <string>
-
-#include "logger.hpp"
-
-int main(int argc, char *argv[]) {
-
-    log("Hello","world!");
     
+    log("Hello","world!");
+
     std::cin.get();
     return EXIT_SUCCESS;
->>>>>>> c369ba5835ad12c5ffaac0e9776d977ff32c9ea4
 }
