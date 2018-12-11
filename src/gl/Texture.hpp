94fa644414001730edcbb158a1f6ca510137dac8
DiD3n <den190242@gmail.com>
2018-12-11 21:09:27 +0100

    Texture class concept

---
+++ src/gl/Texture.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+
+#define GL_TEXTURE_2D 0x0DE1
+
+
+namespace gl {
+
+    class Texture {
+    private:
+        unsigned int id;
+        const std::string path;
+        bool legit;
+
+        bool load();
+
+    public:
+        Texture(const std::string& path);
+        Texture(const Texture& other);
+        ~Texture();
+
+        bool reload();
+
+        constexpr bool isLegit() const {return legit;}
+
+        inline void bind()   const {if (legit) glBindTexture(GL_TEXTURE_2D,id);};
+        inline void unBind() const {if (legit) glBindTexture(GL_TEXTURE_2D, 0);};
+
+    };
+    
+};
