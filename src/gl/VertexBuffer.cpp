a9e1e4261e106e809250391489c621d11ee5d350
DiD3n <den190242@gmail.com>
2018-12-12 19:11:51 +0100

    VertexBuffed::bind() - fix

--- src/gl/VertexBuffer.cpp
+++ src/gl/VertexBuffer.cpp
@@ -28,11 +28,15 @@ namespace gl {
     }
 
     VertexBuffer& VertexBuffer::bind() {
+        int size = 0;
+        for (const gl::LayoutElement& i : vbl->list) {
+            size += i.count * i.getTypeSize();
+        } 
         glBindBuffer(GL_ARRAY_BUFFER, BufferID);
         glBufferData(GL_ARRAY_BUFFER, dataSize, data, GL_DYNAMIC_DRAW);
         int a = 0;
         for (int i = 0; i < vbl->list.size(); i++) {
-            glVertexAttribPointer(i , vbl->list[i].count , vbl->list[i].type , (int)vbl->list[i].normalized , 5 * sizeof(float) , (const void*)a);
+            glVertexAttribPointer(i , vbl->list[i].count , vbl->list[i].type , (int)vbl->list[i].normalized , size , (const void*)a);
             glEnableVertexAttribArray(i);
             a += vbl->list[i].count * vbl->list[i].getTypeSize();
         }
