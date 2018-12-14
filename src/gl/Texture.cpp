36984775723d03e6fc217e0b0abe12bc2ed869ed
DiD3n <den190242@gmail.com>
2018-12-14 23:13:35 +0100

    Small changes in loading textures;

--- src/gl/Texture.cpp
+++ src/gl/Texture.cpp
@@ -6,7 +6,7 @@
 
 namespace gl {
     Texture::Texture(const std::string& path)
-     : path(path){
+     : path(path) {
         glGenTextures(1,&id);
         legit = this->load();
     }
@@ -27,14 +27,30 @@ namespace gl {
             logError("gl::Texture::load(",path,") - IMG_Load");
         } 
         else {
-            this->bind();
+            SDL_Surface* tmpSurface = NULL;
+            tmpSurface = SDL_CreateRGBSurfaceWithFormat(0, surface->w, surface->h, 32, SDL_PIXELFORMAT_RGBA32);
+            if (tmpSurface != NULL) {
+
+                for (int x = 0; x < surface->w; x++) {
+                    for (int y = 0; y < surface->h; y++) {
+                        Uint32 * tmpPixels = (Uint32 *)tmpSurface->pixels;
+                        Uint32 * imgPixels = (Uint32 *)surface->pixels;
+
+                        tmpPixels[tmpSurface->w*y + x] = imgPixels[tmpSurface->w*-(y+1-surface->h) + x];
+                    }
+                }
+            } 
+            else {
+                tmpSurface = surface;
+            }
     
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
             
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w,surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels );
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, surface->w,surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, tmpSurface->pixels );
+            SDL_FreeSurface(tmpSurface);
         }   
 
         SDL_FreeSurface(surface);
