024e33da1a4ad14c3873fde937a8f123424978e7
DiD3n <den190242@gmail.com>
2018-12-19 16:28:31 +0100

    Add GLCall for openGL Functions

--- src/logger.hpp
+++ src/logger.hpp
@@ -3,6 +3,14 @@
 #include <iostream>
 #include <sstream>
 #include "include/sdl2.hpp"
+#include "include/glew.hpp"
+
+//#define GL_OUT_OF_MEMORY 0x0505
+//#define GL_INVALID_FRAMEBUFFER_OPERATION 0x0506
+//#define GL_INVALID_OPERATION 0x0502
+//#define GL_INVALID_VALUE 0x0501
+//#define GL_INVALID_ENUM 0x0500
+//#define GL_NO_ERROR 0
 
 #define DEBUG
 
@@ -15,16 +23,26 @@ SDL_ClearError();\
 }else \
 _raw(args, " at", __FILE__, "\b:",__LINE__)
 
+#define GLCall(x) while(glGetError() != GL_NO_ERROR) {}\
+x;\
+while (unsigned int code = glGetError())\
+_raw( #x, "-" , getErrorMsg(code), "at", __FILE__, "\b:",__LINE__)\
+
 #else
-#define log(args...);
+#define logInfo(args...);
 #define logError(args...);
+#define GLCall(x) x
 #endif
+
+std::string getErrorMsg(unsigned int code);
+
 
 #define _OUT_LOG std::cout
 
 template<typename T>
 static inline void _print(const T& arg) {
     _OUT_LOG << arg << '\n';
+
 }
 
 template<typename T, typename ... T2>
