30f48e5d32808ee84cdea40f5bd1bb18f75e44dd
DiD3n <den190242@gmail.com>
2019-01-28 23:30:04 +0100

    Add if;

--- res/basicFragment.glsl
+++ res/basicFragment.glsl
@@ -8,5 +8,9 @@ in vec2 v_cord;
 in vec3 v_color;
 
 void main() {
-    color = texture(tex,v_cord) * vec4(v_color,1.0f);
+
+    if (v_color != vec3(0.f))
+        color = texture(tex,v_cord) * vec4(v_color,1.0f);
+    else
+        color = texture(tex,v_cord);
 }
