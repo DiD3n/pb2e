5217945cc527ae1b2de58197bd62d9d990e917f0
DiD3n <den190242@gmail.com>
2019-02-02 15:42:22 +0100

    Update gui::Style;

--- src/gui/style.hpp
+++ src/gui/style.hpp
@@ -5,11 +5,26 @@
 
 namespace gui {
 
-	class Style { //0.1v
-	public:
+	struct Style { //0.7v
 		std::string name = "Flat Blue";
-		gl::Color primary = {(ubyte)76, 209, 55}, secondary = {(ubyte)68, 189, 50}, background= {(ubyte)47, 54, 64};
-		gl::Color textColor = {(ubyte)245, 246, 250};
+
+		//colors
+		gl::Color primary    = { (ubyte) 76 , 209, 55 }, 
+		          secondary  = { (ubyte) 68 , 189, 50 }, 
+				  background = { (ubyte) 47 , 54 , 64 },
+				  textColor  = { (ubyte) 245, 246, 250};
+
+		//shader
+		gl::Shader shader;
+
+		//shader
+		gl::VertexBufferLayout vbl;
+
+		Style(const std::string& vertShaderPath = "res/basicUIVert.glsl", const std::string& fragShaderPath = "res/basicUIFrag.glsl") 
+		: shader(vertShaderPath, fragShaderPath) {vbl.push({2}).push({3});}
+		Style(const std::string& fragShaderPath, const std::string& vertShaderPath, const gl::VertexBufferLayout& vbl) 
+		: shader(fragShaderPath, vertShaderPath) , vbl(vbl) {}
+
 	};
 
 };
