2811dd7440afed9a453a234c86cf08a7d067e144
DiD3n <den190242@gmail.com>
2019-02-14 17:23:49 +0100

    Improve gl::Rect

--- src/gl/RectType.hpp
+++ src/gl/RectType.hpp
@@ -10,25 +10,18 @@ namespace gl {
         public:
         T x, y, w, h;
 
-        constexpr gl::Vector2<T>& xy() {
-            return *(gl::Vector2<T>*)&x;
-        }
-        constexpr gl::Vector2<T>& wh() {
-            return *(gl::Vector2<T>*)&w;
-        }
-
-        constexpr const gl::Vector2<T>& xy() const{
-            return *(gl::Vector2<T>*)&x;
-        }
-        constexpr const gl::Vector2<T>& wh() const{
-            return *(gl::Vector2<T>*)&w;
-        }
+        constexpr gl::Vector2<T>& xy() {return *(gl::Vector2<T>*)&x;}
+        constexpr const gl::Vector2<T>& xy() const {return *(gl::Vector2<T>*)&x;}
+        constexpr gl::Vector2<T>& wh() {return *(gl::Vector2<T>*)&w;}
+        constexpr const gl::Vector2<T>& wh() const {return *(gl::Vector2<T>*)&w;}
 
         /* clockwise from the top left */
         constexpr std::array<Vector2<T>,4> getVertices() const {
             return { Vector2<T>(x , y), Vector2<T>(x + w, y), Vector2<T>(x + w, y + h), Vector2<T>(x, y + h) }; 
         }
         operator std::array<Vector2<T>,4>() { return getVertices(); }
+        template<typename T2>
+        operator Rect<T2>() { return {(T2)x, (T2)y, (T2)w, (T2)h}; }
 
         constexpr Rect(const T& a = 0)
          : x(a) , y(a) , w(a) , h(a) {}
@@ -37,13 +30,18 @@ namespace gl {
         constexpr Rect(const Rect& other)
          : x(other.x) , y(other.y), w(other.w) , h(other.h) {}
 
-        Rect operator +(const Rect& other) const { return Rect(x + other.x, y + other.y, h + other.h, w + other.w); }
-        Rect operator -(const Rect& other) const { return Rect(x - other.x, y - other.y, h - other.h, w - other.w); }
-        Rect operator *(const Rect& other) const { return Rect(x * other.x, y * other.y, h * other.h, w * other.w); }
-        Rect operator /(const Rect& other) const { return Rect(x / other.x, y / other.y, h / other.h, w / other.w); }
+        constexpr Rect operator +(const Rect& other) const { return Rect(x + other.x, y + other.y, h + other.h, w + other.w); }
+        constexpr Rect operator -(const Rect& other) const { return Rect(x - other.x, y - other.y, h - other.h, w - other.w); }
+        constexpr Rect operator *(const Rect& other) const { return Rect(x * other.x, y * other.y, h * other.h, w * other.w); }
+        constexpr Rect operator /(const Rect& other) const { return Rect(x / other.x, y / other.y, h / other.h, w / other.w); }
+
+        constexpr Rect operator +(const T& val) const { return Rect(x + val, y + val, h + val, w + val); }
+        constexpr Rect operator -(const T& val) const { return Rect(x - val, y - val, h - val, w - val); }
+        constexpr Rect operator *(const T& val) const { return Rect(x * val, y * val, h * val, w * val); }
+        constexpr Rect operator /(const T& val) const { return Rect(x / val, y / val, h / val, w / val); }
 
 
-        Rect& operator =(const Rect& other) { 
+        constexpr Rect& operator =(const Rect& other) { 
             x = other.x; 
             y = other.y; 
             w = other.w; 
@@ -51,34 +49,24 @@ namespace gl {
             return *this; 
         }
 
-        Rect& operator +=(const Rect& other) { 
-            x += other.x; 
-            y += other.y; 
-            w += other.w; 
-            h += other.h; 
-            return *this; 
+
+        constexpr Rect& operator +=(const Rect& other) { 
+            return *this = (*this) + other;
         }
-        Rect& operator -=(const Rect& other) { 
-            x -= other.x; 
-            y -= other.y; 
-            w -= other.w; 
-            h -= other.h; 
-            return *this; 
+
+        constexpr Rect& operator -=(const Rect& other) { 
+            return *this = (*this) - other;
         }
-        Rect& operator *=(const Rect& other) { 
-            x *= other.x; 
-            y *= other.y; 
-            w *= other.w; 
-            h *= other.h; 
-            return *this; 
+
+        constexpr Rect& operator *=(const Rect& other) { 
+            return *this = (*this) * other;
         }
-        Rect& operator /=(const Rect& other) { 
-            x /= other.x; 
-            y /= other.y; 
-            w /= other.w; 
-            h /= other.h; 
-            return *this; 
+
+        constexpr Rect& operator /=(const Rect& other) { 
+            return *this = (*this) / other; 
         }
+
+
 
         constexpr bool operator ==(const Rect& other) const {
             return (x == other.x && y == other.y && w == other.w && h == other.h);
